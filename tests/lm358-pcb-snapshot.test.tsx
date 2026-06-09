32188fa1c73716339474675d258dc7250aee6640
techmannih <manishchaudharyttt@gmail.com>
2026-06-09 08:41:42 +0530

    use fake server for the test

--- tests/lm358-pcb-snapshot.test.tsx
+++ tests/lm358-pcb-snapshot.test.tsx
@@ -1,8 +1,8 @@
 import { expect, test } from "bun:test"
 import "bun-match-svg"
-import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
-import { convertCircuitJsonToSchematicSvg } from "circuit-to-svg"
-import JSZip from "jszip"
+import {
+  convertCircuitJsonToPcbSvg,
+} from "circuit-to-svg"
 import { RootCircuit } from "tscircuit"
 
 import { createTiPlatformConfig } from "@tscircuit/ti-parts-engine"
@@ -10,42 +10,8 @@ import { createTiPlatformConfig } from "@tscircuit/ti-parts-engine"
 import { LM358 } from "../index"
 import { getTestServer } from "./fixtures/get-test-server"
 
-const LM358_TEST_FOOTPRINT = `(footprint "DIP8_300"
-  (version 20240108)
-  (generator "tscircuit-test")
-  (layer "F.Cu")
-  (attr through_hole)
-  (fp_text reference "\${REFERENCE}" (at 3.81 -2.33 0) (layer "F.SilkS")
-    (effects (font (size 1 1) (thickness 0.15))))
-  (fp_text value "DIP8_300" (at 3.81 9.95 0) (layer "F.Fab")
-    (effects (font (size 1 1) (thickness 0.15))))
-  (fp_line (start 0.635 -1.27) (end 6.985 -1.27) (stroke (width 0.12) (type solid)) (layer "F.SilkS"))
-  (fp_line (start 6.985 -1.27) (end 6.985 8.89) (stroke (width 0.12) (type solid)) (layer "F.SilkS"))
-  (fp_line (start 6.985 8.89) (end 0.635 8.89) (stroke (width 0.12) (type solid)) (layer "F.SilkS"))
-  (fp_line (start 0.635 8.89) (end 0.635 7.62) (stroke (width 0.12) (type solid)) (layer "F.SilkS"))
-  (fp_line (start 0.635 6.35) (end 0.635 5.08) (stroke (width 0.12) (type solid)) (layer "F.SilkS"))
-  (fp_line (start 0.635 3.81) (end 0.635 2.54) (stroke (width 0.12) (type solid)) (layer "F.SilkS"))
-  (fp_line (start 0.635 1.27) (end 0.635 -1.27) (stroke (width 0.12) (type solid)) (layer "F.SilkS"))
-  (pad "1" thru_hole rect (at 0 0 0) (size 1.52 1.52) (drill 0.81) (layers "*.Cu" "*.Mask"))
-  (pad "2" thru_hole circle (at 0 2.54 0) (size 1.52 1.52) (drill 0.81) (layers "*.Cu" "*.Mask"))
-  (pad "3" thru_hole circle (at 0 5.08 0) (size 1.52 1.52) (drill 0.81) (layers "*.Cu" "*.Mask"))
-  (pad "4" thru_hole circle (at 0 7.62 0) (size 1.52 1.52) (drill 0.81) (layers "*.Cu" "*.Mask"))
-  (pad "5" thru_hole circle (at 7.62 7.62 0) (size 1.52 1.52) (drill 0.81) (layers "*.Cu" "*.Mask"))
-  (pad "6" thru_hole circle (at 7.62 5.08 0) (size 1.52 1.52) (drill 0.81) (layers "*.Cu" "*.Mask"))
-  (pad "7" thru_hole circle (at 7.62 2.54 0) (size 1.52 1.52) (drill 0.81) (layers "*.Cu" "*.Mask"))
-  (pad "8" thru_hole circle (at 7.62 0 0) (size 1.52 1.52) (drill 0.81) (layers "*.Cu" "*.Mask"))
-)`
-
-test("LM358 renders a stable PCB snapshot through the TI footprint loader", async () => {
-  const archive = new JSZip()
-  archive.file(
-    "KiCADv6/footprints.pretty/LM358_Test.kicad_mod",
-    LM358_TEST_FOOTPRINT,
-  )
-  const archiveBuffer = await archive.generateAsync({ type: "uint8array" })
-  const { fakeUlProxyUrl, capturedRequests } = await getTestServer({
-    archiveResponseBody: archiveBuffer,
-  })
+test("LM358 renders stable PCB and schematic snapshots", async () => {
+  const { fakeUlProxyUrl, capturedRequests } = await getTestServer()
 
   const circuit = new RootCircuit({
     platform: createTiPlatformConfig({
@@ -62,14 +28,14 @@ test("LM358 renders a stable PCB snapshot through the TI footprint loader", asyn
 
   await circuit.renderUntilSettled()
 
-  const pcbSvg = convertCircuitJsonToPcbSvg(circuit.getCircuitJson())
+  const circuitJson = circuit.getCircuitJson()
+  const pcbSvg = convertCircuitJsonToPcbSvg(circuitJson)
 
   expect(pcbSvg).toContain("<svg")
   expect(pcbSvg).toContain("pcb_plated_hole")
   expect(capturedRequests.some((r) => r.pathname === "/v1/export/kicad")).toBe(
     true,
   )
-  await expect(pcbSvg).toMatchSvgSnapshot(import.meta.path, "lm358-pcb")
-    await expect(pcbSvg).toMatchsc
 
+  await expect(pcbSvg).toMatchSvgSnapshot(import.meta.path, "lm358-pcb")
 })
