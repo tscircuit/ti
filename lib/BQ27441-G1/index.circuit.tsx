66d91ff27dc9ad0507a36cb17d4ab8e25e08b4ed
imrishabh18 <belikerishabh@gmail.com>
2026-06-11 16:05:04 +0530

    updt

--- lib/BQ27441-G1/index.circuit.tsx
+++ lib/BQ27441-G1/index.circuit.tsx
@@ -2,7 +2,7 @@ import { BQ27441DRZR_G1B } from "./imports/BQ27441DRZR_G1B";
 import type { SubcircuitProps } from "@tscircuit/props";
 
 export default (props: SubcircuitProps) => (
-  <subcircuit width={100} height={100} {...props}>
+  <subcircuit schMaxTraceDistance={20} width={100} height={100} {...props}>
     <BQ27441DRZR_G1B
       name="U1"
       schX={0}
