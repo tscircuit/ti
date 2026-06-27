c9ffe63767e6e976a76cb0e7cdcdf4065091b5cc
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2026-06-27 16:33:44 +0530

    Rename the file's to match the component which it exports (#49)

--- index.ts
+++ index.ts
@@ -1,21 +1,21 @@
-import { BatteryManagement_BQ24074 } from "./lib/subcircuits/BQ24074-subcircuit.circuit";
-import { BatteryManagement_BQ25895 } from "./lib/subcircuits/BQ25895-subcircuit.circuit";
-import { BatteryManagement_BQ27441G1 } from "./lib/subcircuits/BQ27441-G1-subcircuit.circuit";
-import { WirelessMCU_CC2340R5 } from "./lib/subcircuits/CC2340R5-subcircuit.circuit";
-import { WirelessMCU_CC3235SF } from "./lib/subcircuits/CC3235SF-subcircuit.circuit";
-import { MotorDriver_DRV8833 } from "./lib/subcircuits/DRV8833-subcircuit.circuit";
-import { MotorDriver_DRV8876 } from "./lib/subcircuits/DRV8876-subcircuit.circuit";
-import { EnvironmentalSensor_HDC2080 } from "./lib/subcircuits/HDC2080-subcircuit.circuit";
-import { EnvironmentalSensor_HDC3020 } from "./lib/subcircuits/HDC3020-subcircuit.circuit";
-import { EnvironmentalSensor_HDC3022 } from "./lib/subcircuits/HDC3022-subcircuit.circuit";
-import { PowerMonitor_INA237 } from "./lib/subcircuits/INA237-subcircuit.circuit";
-import { Microcontroller_MSPM0G3507 } from "./lib/subcircuits/MSPM0G3507-subcircuit.circuit";
-import { TemperatureSensor_TMP1075 } from "./lib/subcircuits/TMP1075-subcircuit.circuit";
-import { LoadSwitch_TPS22919 } from "./lib/subcircuits/TPS22919-subcircuit.circuit";
-import { BuckConverter_TPS62933 } from "./lib/subcircuits/TPS62933-subcircuit.circuit";
-import { BuckBoostConverter_TPS63802 } from "./lib/subcircuits/TPS63802-subcircuit.circuit";
-import { LDO_TPS7A02 } from "./lib/subcircuits/TPS7A02-subcircuit.circuit";
-import { PowerModule_TPSM82823 } from "./lib/subcircuits/TPSM82823-subcircuit.circuit";
+import { BatteryManagement_BQ24074 } from "./lib/subcircuits/BatteryManagement_BQ24074.circuit";
+import { BatteryManagement_BQ25895 } from "./lib/subcircuits/BatteryManagement_BQ25895.circuit";
+import { BatteryManagement_BQ27441G1 } from "./lib/subcircuits/BatteryManagement_BQ27441G1.circuit";
+import { WirelessMCU_CC2340R5 } from "./lib/subcircuits/WirelessMCU_CC2340R5.circuit";
+import { WirelessMCU_CC3235SF } from "./lib/subcircuits/WirelessMCU_CC3235SF.circuit";
+import { MotorDriver_DRV8833 } from "./lib/subcircuits/MotorDriver_DRV8833.circuit";
+import { MotorDriver_DRV8876 } from "./lib/subcircuits/MotorDriver_DRV8876.circuit";
+import { EnvironmentalSensor_HDC2080 } from "./lib/subcircuits/EnvironmentalSensor_HDC2080.circuit";
+import { EnvironmentalSensor_HDC3020 } from "./lib/subcircuits/EnvironmentalSensor_HDC3020.circuit";
+import { EnvironmentalSensor_HDC3022 } from "./lib/subcircuits/EnvironmentalSensor_HDC3022.circuit";
+import { PowerMonitor_INA237 } from "./lib/subcircuits/PowerMonitor_INA237.circuit";
+import { Microcontroller_MSPM0G3507 } from "./lib/subcircuits/Microcontroller_MSPM0G3507.circuit";
+import { TemperatureSensor_TMP1075 } from "./lib/subcircuits/TemperatureSensor_TMP1075.circuit";
+import { LoadSwitch_TPS22919 } from "./lib/subcircuits/LoadSwitch_TPS22919.circuit";
+import { BuckConverter_TPS62933 } from "./lib/subcircuits/BuckConverter_TPS62933.circuit";
+import { BuckBoostConverter_TPS63802 } from "./lib/subcircuits/BuckBoostConverter_TPS63802.circuit";
+import { PowerManagement_TPS7A02 } from "./lib/subcircuits/PowerManagement_TPS7A02.circuit";
+import { PowerModule_TPSM82823 } from "./lib/subcircuits/PowerModule_TPSM82823.circuit";
 
 export {
   BatteryManagement_BQ24074,
@@ -34,7 +34,7 @@ export {
   LoadSwitch_TPS22919,
   BuckConverter_TPS62933,
   BuckBoostConverter_TPS63802,
-  LDO_TPS7A02,
+  PowerManagement_TPS7A02,
   PowerModule_TPSM82823,
 };
 
@@ -55,7 +55,7 @@ export const TiSubcircuitComponents = {
   LoadSwitch_TPS22919,
   BuckConverter_TPS62933,
   BuckBoostConverter_TPS63802,
-  LDO_TPS7A02,
+  PowerManagement_TPS7A02,
   PowerModule_TPSM82823,
 } as const;
 
