2a0f327a772d71626123314bd960431d6b04dd3e
Mustafa Mulla <mustafamulla765@gmail.com>
2026-06-30 15:37:21 +0530

    Fix BQ24074 simulation schematic coordinates

--- lib/simulations/BQ24074-dppm-and-battery-supplement.circuit.tsx
+++ lib/simulations/BQ24074-dppm-and-battery-supplement.circuit.tsx
@@ -88,8 +88,8 @@ export const BQ24074DppmAndBatterySupplementCircuit = () => (
       name="U1"
       schX={0}
       schY={0}
-      schWidth={3.2}
-      schHeight={6.5}
+      schWidth={2.05}
+      schHeight={3}
       showPinAliases={false}
       schPinArrangement={{
         leftSide: {
@@ -107,28 +107,46 @@ export const BQ24074DppmAndBatterySupplementCircuit = () => (
         OUT: { marginBottom: 0.8 },
         BAT: { marginBottom: 0.8 },
       }}
+      connections={{
+        IN: "net.VIN",
+        N_CE: "net.GND",
+        EN1: "net.GND",
+        EN2: "net.EN2",
+        TS: "net.TS",
+        VSS: "net.GND",
+        OUT: "net.OUT",
+        BAT: "net.BAT",
+        ISET: "net.ISET",
+        ILIM: "net.ILIM",
+        ITERM: "net.ITERM",
+        TMR: "net.TMR",
+        EP: "net.GND",
+      }}
     />
 
     <voltagesource
       name="V_IN"
       voltage="5V"
-      schX={-6}
-      schY={2.4}
+      schX={-4.4}
+      schY={2.2}
       schRotation="270deg"
+      connections={{ pin1: "net.VIN", pin2: "net.GND" }}
     />
     <voltagesource
       name="V_LOGIC"
       voltage="3.3V"
-      schX={-6}
+      schX={-4}
       schY={-0.8}
       schRotation="270deg"
+      connections={{ pin1: "net.EN2", pin2: "net.GND" }}
     />
     <voltagesource
       name="V_BAT"
       voltage="3.6V"
-      schX={5.8}
-      schY={1}
+      schX={4.2}
+      schY={0.6}
       schRotation="270deg"
+      connections={{ pin1: "net.BAT", pin2: "net.GND" }}
     />
 
     <ammeter
@@ -139,8 +157,8 @@ export const BQ24074DppmAndBatterySupplementCircuit = () => (
       graphCenter={0.1}
       graphOffsetDivs={-2.5}
       graphUnitsPerDiv={0.25}
-      schX={3.6}
-      schY={1}
+      schX={11}
+      schY={3.1}
     />
     <ammeter
       name="IOUT_PROBE"
@@ -150,8 +168,8 @@ export const BQ24074DppmAndBatterySupplementCircuit = () => (
       graphCenter={0.6}
       graphOffsetDivs={6}
       graphUnitsPerDiv={0.25}
-      schX={3.1}
-      schY={2.5}
+      schX={3.8}
+      schY={3.1}
     />
     <ammeter
       name="IIN_PROBE"
@@ -161,121 +179,136 @@ export const BQ24074DppmAndBatterySupplementCircuit = () => (
       graphCenter={0.45}
       graphOffsetDivs={1}
       graphUnitsPerDiv={0.25}
-      schX={3.1}
-      schY={3.5}
+      schX={7.4}
+      schY={3.1}
     />
 
     <BQ24074DppmCurrentWaveform
       name="IOUT_WAVE"
       subcktName="BQ24074_DPPM_IOUT"
       waveform="0 0 2m 0 4m -0.4 8m -0.9 10m -1.2 13m -0.4 15m 0 16m 0"
-      schX={5.2}
-      schY={3.6}
+      schX={3.8}
+      schY={4.5}
       schWidth={1.5}
-      schHeight={1}
+      schHeight={0.4}
+      connections={{ NEG: "net.GND" }}
     />
     <BQ24074DppmCurrentWaveform
       name="IIN_WAVE"
       subcktName="BQ24074_DPPM_IIN"
       waveform="0 -0.5 2m -0.5 4m -0.9 13m -0.9 15m -0.5 16m -0.5"
-      schX={7}
-      schY={3.6}
+      schX={7.4}
+      schY={4.5}
       schWidth={1.5}
-      schHeight={1}
+      schHeight={0.4}
+      connections={{ NEG: "net.GND" }}
     />
     <BQ24074DppmCurrentWaveform
       name="IBAT_WAVE"
       subcktName="BQ24074_DPPM_IBAT"
       waveform="0 -0.5 4m -0.5 8m 0 10m 0.3 11m 0 13m -0.5 16m -0.5"
-      schX={8.8}
-      schY={3.6}
+      schX={11}
+      schY={4.5}
       schWidth={1.5}
-      schHeight={1}
+      schHeight={0.4}
+      connections={{ NEG: "net.GND" }}
     />
 
     <BQ24074DppmRampedLoad
       name="LOAD"
-      schX={5.2}
-      schY={2.5}
+      schX={4.4}
+      schY={1.9}
       schWidth={1.5}
-      schHeight={1.2}
+      schHeight={0.4}
+      connections={{ OUT: "net.OUT", VSS: "net.GND" }}
     />
     <BQ24074DppmVoutFixture
       name="VOUT_FIXTURE"
-      schX={6.9}
-      schY={2.5}
+      schX={7.8}
+      schY={1.9}
       schWidth={1.6}
-      schHeight={1.2}
+      schHeight={0.4}
+      connections={{ OUT: "net.OUT" }}
     />
 
     <capacitor
       name="C_IN"
       capacitance="1uF"
       footprint="0603"
-      schX={-4.4}
-      schY={2.4}
+      schX={-3.1}
+      schY={2.2}
       schOrientation="vertical"
+      connections={{ pin1: "net.VIN", pin2: "net.GND" }}
     />
     <capacitor
       name="C_OUT"
       capacitance="4.7uF"
       footprint="0603"
-      schX={3.5}
-      schY={2.5}
+      schX={2.05}
+      schY={1.05}
       schOrientation="vertical"
+      connections={{ pin1: "net.OUT", pin2: "net.GND" }}
     />
     <resistor
       name="R_ISET"
       resistance="1.78k"
       footprint="0603"
-      schX={3.2}
-      schY={-1.7}
+      schX={6.3}
+      schY={-2.2}
       schOrientation="vertical"
+      connections={{ pin1: "net.ISET", pin2: "net.GND" }}
     />
     <resistor
       name="R_ILIM"
       resistance="1.72k"
       footprint="0603"
-      schX={4.5}
-      schY={-1.7}
+      schX={5}
+      schY={-2.2}
       schOrientation="vertical"
+      connections={{ pin1: "net.ILIM", pin2: "net.GND" }}
     />
     <resistor
       name="R_ITERM"
       resistance="1k"
       footprint="0603"
-      schX={5.8}
-      schY={-1.7}
+      schX={3.7}
+      schY={-2.2}
       schOrientation="vertical"
+      connections={{ pin1: "net.ITERM", pin2: "net.GND" }}
     />
     <resistor
       name="R_TMR"
       resistance="48k"
       footprint="0603"
-      schX={7.1}
-      schY={-1.7}
+      schX={2.4}
+      schY={-2.2}
       schOrientation="vertical"
+      connections={{ pin1: "net.TMR", pin2: "net.GND" }}
     />
     <resistor
       name="R_TS"
       resistance="10k"
       footprint="0603"
-      schX={-3.5}
-      schY={-2.3}
+      schX={-3.2}
+      schY={-1.7}
       schOrientation="vertical"
+      connections={{ pin1: "net.TS", pin2: "net.GND" }}
     />
 
     <schematictext
       text="DPPM and battery supplement setup, VBAT = 3.6 V"
-      schX={-1.5}
-      schY={4}
+      schX={0}
+      schY={2.5}
       fontSize={0.16}
       anchor="center"
     />
-    <netlabel net="VIN" connection="U1.IN" schX={-2.2} schY={2.4} />
-    <netlabel net="BAT" connection="U1.BAT" schX={2.1} schY={1} />
-    <netlabel net="OUT" connection="U1.OUT" schX={2.1} schY={2.4} />
-
+    <netlabel
+      net="GND"
+      connection="VOUT_FIXTURE.VSS"
+      anchorSide="top"
+      schX={10}
+      schY={1.4}
+    />
     <trace from="V_IN.pin1" to="U1.IN" />
     <trace from="V_IN.pin2" to="net.GND" />
     <trace from="V_LOGIC.pin1" to="U1.EN2" />
