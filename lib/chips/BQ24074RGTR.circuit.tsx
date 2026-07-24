79320a6829a2b45b5f2634782a87b43aedf16a7d
techmannih <manishchaudharyttt@gmail.com>
2026-07-24 17:36:24 +0530

    format

--- lib/chips/BQ24074RGTR.circuit.tsx
+++ lib/chips/BQ24074RGTR.circuit.tsx
@@ -272,9 +272,6 @@ export const BQ24074RGTR = (props: ChipProps<typeof pinLabels>) => {
         modelOriginPosition: { x: 0, y: 0.000012699999999199463, z: 0 },
       }}
       {...props}
-
-
-      
     />
   );
 };
