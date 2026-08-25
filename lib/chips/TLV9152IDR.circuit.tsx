5d10c04c5992bc50d5f2bc41b4f7afa8ee8b6e39
Sangit Manandhar <rux12@icloud.com>
2026-08-25 12:45:48 -0700

    fix(chips): name TLV9152 snapshot

--- lib/chips/TLV9152IDR.circuit.tsx
+++ lib/chips/TLV9152IDR.circuit.tsx
@@ -1,8 +1,14 @@
 import type { OpAmpProps } from "@tscircuit/props";
 import "tscircuit";
 
-export const TLV9152IDR = (props: OpAmpProps) => (
+type TLV9152IDRProps = Omit<OpAmpProps, "name"> & { name?: string };
+
+export const TLV9152IDR = ({
+  name = "TLV9152IDR",
+  ...props
+}: TLV9152IDRProps) => (
   <opamp
+    name={name}
     manufacturerPartNumber="TLV9152IDR"
     symbolName="opamp_with_power"
     {...props}
