acafc90519b22175dd19066a76ca6d72962cee1d
Mustafa Mulla <mustafamulla765@gmail.com>
2026-08-28 00:32:00 +0530

    refactor: use direct traces in seat position example

--- examples/SeatPositionModule.circuit.tsx
+++ examples/SeatPositionModule.circuit.tsx
@@ -6,12 +6,7 @@ import {
   PositionFeedback_DRV5013_TIDA01389,
   PowerSupply_LM5050_TIDA00992,
 } from "@tsci/tscircuit.ti";
-import type { TraceProps } from "@tscircuit/props";
 import "tscircuit";
-
-const Interconnect = (props: TraceProps) => (
-  <trace {...props} maxLength="180mm" />
-);
 
 /** Seat Position Module assembled from reusable TI reference subcircuits. */
 export default () => (
@@ -78,51 +73,51 @@ export default () => (
     />
 
     {/* Vehicle input and protected battery rail. */}
-    <Interconnect from=".power > .U1 > .IN" to="net.VBATT" />
-    <Interconnect from=".power > .U1 > .OUT" to="net.VPROTECTED" />
-    <Interconnect from=".motor > .U1 > .PVDD" to="net.VPROTECTED" />
-    <Interconnect from=".light > .R3 > .pin1" to="net.VPROTECTED" />
+    <trace from=".power > .U1 > .IN" to="net.VBATT" />
+    <trace from=".power > .U1 > .OUT" to="net.VPROTECTED" />
+    <trace from=".motor > .U1 > .PVDD" to="net.VPROTECTED" />
+    <trace from=".light > .R3 > .pin1" to="net.VPROTECTED" />
 
     {/* Regulated rails supplied by the surrounding seat module. */}
-    <Interconnect from=".can > .R21 > .pin1" to="net.V5P0" />
-    <Interconnect from=".can > .R22 > .pin1" to="net.V3P3" />
-    <Interconnect from=".mcu > .C14 > .pin2" to="net.V3P3" />
-    <Interconnect from=".position > .U6 > .VCC" to="net.V3P3" />
-    <Interconnect from=".motor > .C18 > .pin1" to="net.V3P3" />
+    <trace from=".can > .R21 > .pin1" to="net.V5P0" />
+    <trace from=".can > .R22 > .pin1" to="net.V3P3" />
+    <trace from=".mcu > .C14 > .pin2" to="net.V3P3" />
+    <trace from=".position > .U6 > .VCC" to="net.V3P3" />
+    <trace from=".motor > .C18 > .pin1" to="net.V3P3" />
 
     {/* Common ground. */}
-    <Interconnect from=".power > .U1 > .GND" to="net.GND" />
-    <Interconnect from=".can > .U6 > .GND" to="net.GND" />
-    <Interconnect from=".mcu > .U6 > .VSS" to="net.GND" />
-    <Interconnect from=".position > .U6 > .GND" to="net.GND" />
-    <Interconnect from=".motor > .U1 > .GND1" to="net.GND" />
+    <trace from=".power > .U1 > .GND" to="net.GND" />
+    <trace from=".can > .U6 > .GND" to="net.GND" />
+    <trace from=".mcu > .U6 > .VSS" to="net.GND" />
+    <trace from=".position > .U6 > .GND" to="net.GND" />
+    <trace from=".motor > .U1 > .GND1" to="net.GND" />
 
     {/* Vehicle CAN bus and MCU interface. */}
-    <Interconnect from=".can > .R23 > .pin1" to="net.CANH" />
-    <Interconnect from=".can > .R24 > .pin2" to="net.CANL" />
-    <Interconnect from=".mcu > .U6 > .PA8" to=".can > .U6 > .TXD" />
-    <Interconnect from=".mcu > .U6 > .PA9" to=".can > .U6 > .RXD" />
-    <Interconnect from=".mcu > .U6 > .PA10" to=".can > .U6 > .STB" />
+    <trace from=".can > .R23 > .pin1" to="net.CANH" />
+    <trace from=".can > .R24 > .pin2" to="net.CANL" />
+    <trace from=".mcu > .U6 > .PA8" to=".can > .U6 > .TXD" />
+    <trace from=".mcu > .U6 > .PA9" to=".can > .U6 > .RXD" />
+    <trace from=".mcu > .U6 > .PA10" to=".can > .U6 > .STB" />
 
     {/* Position feedback, lighting and protected-power control. */}
-    <Interconnect from=".position > .U6 > .OUT" to=".mcu > .U6 > .PA11" />
-    <Interconnect from=".position > .U5 > .OUT" to=".mcu > .U6 > .PA12" />
-    <Interconnect from=".mcu > .U6 > .PA13" to=".light > .R4 > .pin1" />
-    <Interconnect from=".mcu > .U6 > .PA0" to=".power > .R3 > .pin1" />
+    <trace from=".position > .U6 > .OUT" to=".mcu > .U6 > .PA11" />
+    <trace from=".position > .U5 > .OUT" to=".mcu > .U6 > .PA12" />
+    <trace from=".mcu > .U6 > .PA13" to=".light > .R4 > .pin1" />
+    <trace from=".mcu > .U6 > .PA0" to=".power > .R3 > .pin1" />
 
     {/* DRV8305 status, serial interface and three half-bridge inputs. */}
-    <Interconnect from=".motor > .U1 > .nFAULT" to=".mcu > .U6 > .PA1" />
-    <Interconnect from=".mcu > .U6 > .PA3" to=".motor > .U1 > .WAKE" />
-    <Interconnect from=".mcu > .U6 > .PA4" to=".motor > .U1 > .nSCS" />
-    <Interconnect from=".mcu > .U6 > .PA5" to=".motor > .U1 > .SCLK" />
-    <Interconnect from=".mcu > .U6 > .PA6" to=".motor > .U1 > .SDI" />
-    <Interconnect from=".motor > .U1 > .SDO" to=".mcu > .U6 > .PA7" />
-    <Interconnect from=".mcu > .U6 > .PA21" to=".motor > .U1 > .INHA" />
-    <Interconnect from=".mcu > .U6 > .PA22" to=".motor > .U1 > .INLA" />
-    <Interconnect from=".mcu > .U6 > .PA23" to=".motor > .U1 > .INHB" />
-    <Interconnect from=".mcu > .U6 > .PA24" to=".motor > .U1 > .INLB" />
-    <Interconnect from=".mcu > .U6 > .PA25" to=".motor > .U1 > .INHC" />
-    <Interconnect from=".mcu > .U6 > .PA26" to=".motor > .U1 > .INLC" />
-    <Interconnect from=".mcu > .U6 > .PA27" to=".motor > .U1 > .EN_GATE" />
+    <trace from=".motor > .U1 > .nFAULT" to=".mcu > .U6 > .PA1" />
+    <trace from=".mcu > .U6 > .PA3" to=".motor > .U1 > .WAKE" />
+    <trace from=".mcu > .U6 > .PA4" to=".motor > .U1 > .nSCS" />
+    <trace from=".mcu > .U6 > .PA5" to=".motor > .U1 > .SCLK" />
+    <trace from=".mcu > .U6 > .PA6" to=".motor > .U1 > .SDI" />
+    <trace from=".motor > .U1 > .SDO" to=".mcu > .U6 > .PA7" />
+    <trace from=".mcu > .U6 > .PA21" to=".motor > .U1 > .INHA" />
+    <trace from=".mcu > .U6 > .PA22" to=".motor > .U1 > .INLA" />
+    <trace from=".mcu > .U6 > .PA23" to=".motor > .U1 > .INHB" />
+    <trace from=".mcu > .U6 > .PA24" to=".motor > .U1 > .INLB" />
+    <trace from=".mcu > .U6 > .PA25" to=".motor > .U1 > .INHC" />
+    <trace from=".mcu > .U6 > .PA26" to=".motor > .U1 > .INLC" />
+    <trace from=".mcu > .U6 > .PA27" to=".motor > .U1 > .EN_GATE" />
   </board>
 );
