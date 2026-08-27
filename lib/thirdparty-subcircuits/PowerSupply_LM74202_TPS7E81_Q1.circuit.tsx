5dadcb5e3c30c4b9f6032d905f7c97470f442ec1
techmannih <manishchaudharyttt@gmail.com>
2026-08-27 11:17:46 +0530

    feat: connect rearview mirror module example

--- lib/thirdparty-subcircuits/PowerSupply_LM74202_TPS7E81_Q1.circuit.tsx
+++ lib/thirdparty-subcircuits/PowerSupply_LM74202_TPS7E81_Q1.circuit.tsx
@@ -97,7 +97,7 @@ export const PowerSupply_LM74202_TPS7E81_Q1 = (props: SubcircuitProps) => (
     <diode
       name="TVS"
       manufacturerPartNumber="SMBJ28CA"
-      footprint="do214aa"
+      footprint="smb"
       variant="tvs"
       schX={-9.5}
       schY={-1.6}
